3d6c824259fdf4e9e78e3138d2c9c0fecedf645f
Graham <graham.montgomery@western.edu>
2015-06-07 14:27:11 -0600

    Oops, nevermind. there is the problem

--- WindowsInstall.cmd
+++ WindowsInstall.cmd
@@ -1,5 +1,5 @@
 copy Constants.py C:\Python33\Lib\Constants.py
-copy cs0.py C:\Python33\Lib\StartPy.py
+copy StartPy.py C:\Python33\Lib\StartPy.py
 copy Engine.py C:\Python33\Lib\Engine.py
 copy Events.py C:\Python33\Lib\Events.py
 copy Globals.py C:\Python33\Lib\Globals.py
